91431118b3654309475af29da26b596d7d45c7b4
Annisa Kurniawati <annisak0708@gmail.com>
2025-03-21 06:10:48 +0700

    kategori dan barang

--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -6,7 +6,9 @@
     <div class="card-header">
         <h3 class="card-title">{{ $page->title }}</h3>
         <div class="card-tools">
-            <a class="btn btn-sm btn-primary mt-1" href="{{ url('kategori/create') }}">Tambah</a>
+            <a class="btn btn-sm btn-primary mt-1" href="{{ url('barang/create') }}">Tambah</a>
+            <button onclick="modalAction('{{ url('barang/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah
+                Ajax</button>
         </div>
     </div>
     <div class="card-body">
@@ -16,65 +18,128 @@
         @if (session('error'))
         <div class="alert alert-danger">{{ session('error') }}</div>
         @endif
-        <table class="table table-bordered table-striped table-hover table-sm" id="table_kategori">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="form-group row">
+                    <label class="col-1 control-label col-form-label">Filter:</label>
+                    <div class="col-3">
+                        <select class="form-control" id="kategori_id" name="kategori_id" required>
+                            <option value="">- Semua -</option>
+                            @foreach($kategori as $item)
+                            <option value="{{ $item->kategori_id }}">{{ $item->kategori_nama }}</option>
+                            @endforeach
+                        </select>
+                        <small class="form-text text-muted">Kategori Barang</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <table class="table table-bordered table-striped table-hover table-sm" id="table_barang">
             <thead>
                 <tr>
                     <th>ID</th>
                     <th>Kode</th>
                     <th>Nama</th>
+                    <th>Harga Beli</th>
+                    <th>Harga Jual</th>
+                    <th>Kategori Barang</th>
                     <th>Aksi</th>
                 </tr>
             </thead>
         </table>
     </div>
 </div>
+<div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" databackdrop="static"
+data-keyboard="false" data-width="75%" aria-hidden="true"></div>
 
 @endsection
 
 @push('css')
 @endpush
+
 @push('js')
 
 <script>
-    $(document).ready(function() {
-      var dataKategori = $('#table_kategori').DataTable({
-          // serverSide: true, jika ingin menggunakan server side processing
-          serverSide: true,
-          ajax: {
-              "url": "{{ url('kategori/list') }}",
-              "dataType": "json",
-              "type": "POST",
-          },
-          columns: [
-            {
-                // nomor urut dari laravel datatable addIndexColumn()
-                data: "DT_RowIndex",
-                className: "text-center",
-                orderable: false,
-                searchable: false
+    function modalAction(url = ''){
+        $('#myModal').load(url,function(){
+            $('#myModal').modal('show');
+        });
+    }
+    
+    var dataUser;
+    $(document).ready(function () {
+        dataUser = $('#table_barang').DataTable({
+            // serverSide: true, jika ingin menggunakan server side processing
+            serverSide: true,
+            ajax: {
+                "url": "{{ url('barang/list') }}",
+                "dataType": "json",
+                "type": "POST",
+                "data": function (d) {
+                        d.kategori_id = $('#kategori_id').val();
+                }
             },
+            columns: [
+                {
+                    // nomor urut dari laravel datatable addIndexColumn()
+                    data: "DT_RowIndex",
+                    className: "text-center",
+                    orderable: false,
+                    searchable: false
+                },
 
-            {
-                data: "kategori_kode",
-                className: "",
-                orderable: true,
-                searchable: true
-            },
+                {
+                    data: "barang_kode",
+                    className: "",
+                    orderable: true,
+                    searchable: true
+                },
 
-            {
-                data: "kategori_nama",
-                className: "",
-                orderable: true,
-                searchable: true
-            },
+                {
+                    data: "barang_nama",
+                    className: "",
+                    orderable: true,
+                    searchable: true
+                },
 
-            {
-                data: "aksi",
-                className: "",
-                orderable: false,
-                searchable: false
-            }
-        ]
+                {
+                    data: "harga_beli",
+                    className: "",
+                    orderable: false,
+                    searchable: false,
+                    render: function (data, type, row) {
+                        return "Rp " + new Intl.NumberFormat("id-ID").format(data);
+                    }
+                },
+
+                {
+                    data: "harga_jual",
+                    className: "",
+                    orderable: false,
+                    searchable: false,
+                    render: function (data, type, row) {
+                        return "Rp " + new Intl.NumberFormat("id-ID").format(data);
+                    }
+                },
+
+                {
+                    data: "kategori.kategori_nama",
+                    className: "",
+                    orderable: false,
+                    searchable: false
+                },
+
+                {
+                    data: "aksi",
+                    className: "",
+                    orderable: false,
+                    searchable: false
+                }
+            ]
+        });
+
+    $('#kategori_id').change(function() {
+        dataBarang.ajax.reload();
     });
 });
 </script>
